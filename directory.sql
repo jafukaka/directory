86549144f054835956dae9ab0001fc0a5048e396
jafukaka <jafukaka@gmail.com>
2021-03-25 10:27:38 +0530

    Add files via upload

---
+++ directory.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 25, 2021 at 04:56 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `directory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload`
+--
+
+DROP TABLE IF EXISTS `upload`;
+CREATE TABLE IF NOT EXISTS `upload` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` text NOT NULL,
+  `uploadtime` datetime DEFAULT NULL,
+  `deletetime` datetime DEFAULT NULL,
+  `status` tinyint(4) NOT NULL COMMENT '0-delete,1-active',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
